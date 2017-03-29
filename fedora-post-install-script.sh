96131b6194919652b129b0037db10981bd1dd095
Taylor Clayton Frey <taylor.frey@gmail.com>
2017-03-28 20:23:59 -0600

    Added note to 'Install TOML package' for sublime

--- fedora-post-install-script.sh
+++ fedora-post-install-script.sh
@@ -71,6 +71,7 @@ sudo cp -r /opt/sublime_text/Icon/256x256/* /usr/share/icons/hicolor/256x256/app
 # Enter purchase license stored in Enpass
 # Install Package Control from https://packagecontrol.io/
 # User preferences on Github
+# Install TOML package
 
 #### GOOGLE CHROME BROWSER ####
 # Download Google Chrome Browser from https://www.google.com/chrome/browser/desktop/index.html
@@ -194,3 +195,5 @@ npm install --save-dev babel-cli babel-preset-env babel-register
 # Assuming the GOPATH is set and working correctly
 go get -v github.com/spf13/hugo
 # Ensure the $HOME/go/bin is part of the $PATH in the .bash_profile
+
+
